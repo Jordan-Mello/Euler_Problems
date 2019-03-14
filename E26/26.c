4ac242de57cbcbac6657dcc6e36b19c0a3d3e504
Jordan-Mello <47681832+Jordan-Mello@users.noreply.github.com>
2019-03-14 12:28:34 -0400

    large num (1,000,002)

--- E26/26.c
+++ E26/26.c
@@ -1,30 +1,36 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-void printArray(float *initialArray, int i);
+void inputF(unsigned long int input);
+
 int main(void)
 {
+    unsigned long int input = 0;
 
-    float l = 1000000000;
-    float all[1000];
-    float in = 0;
-    for(int i = 0; i < 1000; i++)
-    {
+    inputF(input);
 
-        all[i] = l / in;
-        in++;
-    }
-    printArray(all, 1000);
     return 0;
 }
-void printArray(float *initialArray, int i)
+void inputF(unsigned long int input)
 {
-    printf("Array: ");
-    for(int k = 0; k < i ; k++)
+    char *temp = malloc(sizeof(char) * 15);
+    printf("Phone number (enter only numbers):");
+
+    fgets(temp, 15, stdin); //tried scanf, it sucks tbh for stuff like this
+    if(temp[0] == '\n') //you can add extra conditions if you need (this just detects enter)
     {
-        printf("%d: %f\n", k, initialArray[k]);
+        inputF(input);
     }
-    printf("\n");
+    else
+    {
+        input = strtoul(temp, &temp, 10); //makes string to int (technically integer == unsigned long int)
+        if(input != 0)
+        {
+            printf("%lu", input); //you can check for negatives if need be
+        }
+    }
+    free(temp);
+
+
 }
